d434fcce5dcfac84ef2cfbd03285b51ed1ec32c2
Yaroslav Nol <yaroslavnol@gmail.com>
2025-09-19 19:16:15 +0200

    1

--- 3.py
+++
@@ -1,5 +0,0 @@
-# 3
-
-for i in range(1, 101):  
-    if i % 2 == 0 or i % 3 == 0:
-        print(i, end=" ")  
--- 4.py
+++
@@ -1,4 +0,0 @@
-# 4 
-n = int(input("\n Введіть кількість чисел: "))
-for i in range(n):
-    print(2 ** i, end=" ")
